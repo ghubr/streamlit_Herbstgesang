b8147f8e3c04620e99e8df8c33a6fea70eb5be11
GH diamond <ger.huber@gmx.de>
2025-01-05 22:05:51 +0100

    Updated with Phänologie

--- src/st_Herbstgesang.py
+++ src/st_Herbstgesang.py
@@ -14,8 +14,6 @@ import streamlit as st
 import os.path
 from config import pathToData
 
-# csv can be found here
-#pathToData = r'E:\\daten\\Projekte\\2021-10-15 oval_referenz\\output\\Herbstgesang'
 
 map_months = { i:["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"][i-1] for i in range(1,13) }
 
@@ -39,6 +37,7 @@ with tab1:
     # st.image(os.path.join(pathToData, "Singsang-Meldungen_pro_Jahr.png"))
     st.text("Gesangs-Meldungen (Meldungen mit 'Gesang' o.ä. im Kommentar) pro Jahr. Das Projekt wurde erstmals 2024 durchgeführt")
     
+    
     df = pd.read_csv(os.path.join(pathToData, "stdat_Meldungen_pro_Monat.csv"))
     st.header("Gesangs-Meldungen pro Monat")
     # make year categorical
@@ -48,6 +47,15 @@ with tab1:
     # st.image(os.path.join(pathToData, "Singsang-Meldungen_je_Monat.png"))
     st.text("Gesangs-Meldungen je Monat. Gesangs-Kommentare wurden 12/2023 vorgestellt. Das Projekt begann 8/2024.")
     
+    
+    st.header("Gesangs-Meldungen je Art (Aug-Dez) - nur Arten mit min. 10 Meldungen.")
+    df = pd.read_csv(os.path.join(pathToData, "stdat_Beobachtungen_pro_Art_und_Jahr.csv"))
+   
+    df_sum = df.groupby('TAXON')[['OBSERVATIONS']].agg('sum').sort_values(by='OBSERVATIONS', ascending=False).reset_index()
+    st.bar_chart(df_sum[df_sum.OBSERVATIONS>=10], x='TAXON', y='OBSERVATIONS', horizontal=True)
+    
+    df["DATE_YEAR"] = pd.Categorical(df.DATE_YEAR)
+    st.bar_chart(df, x='TAXON', y='OBSERVATIONS', color='DATE_YEAR', stack=True, horizontal=True)
     
 with tab2:
     st.header("Anzahl der Melder mit Gesangs-Meldungen")
@@ -68,33 +76,37 @@ with tab3:
     # df['DATE_MONTH'] = df['DATE_MONTH'].map(map_months)
     st.bar_chart(df, x='DATE_MONTH', stack=False)
     
+    
 with tab4:
     st.header("Phänologien")
+    
+    obs_count_min = 20
     
     # Read data
     df = pd.read_csv(os.path.join(pathToData, "stdat_Meldungen_je_Art_und_Monat.csv"))
     # df = pd.read_csv(os.path.join(pathToData, "stdat_Phaenologien_der_Arten.csv"))
     species_available = df.TAXON.unique()
     
-    species = df.groupby('TAXON').OBSERVATIONS.sum()
-    st.dataframe(species)
-    species = species[species>10].index.to_numpy()
+    species = df.groupby('TAXON').OBSERVATIONS.sum().sort_values(ascending=False)
+    st.dataframe(species, width=600)
+    species = species[species>=obs_count_min]
     
-    option_art = st.selectbox(
-        "Art wählen:",
-        species
-    )
-    st.write("Phänologie der Gesangsmeldungen (Aug-Dez) für", option_art, ".")
+    # tx = st.selectbox(
+    #     "Art wählen:",
+    #     species.index.to_numpy()
+    # )
     
-    if option_art in species_available:
-        d = df[df.TAXON==option_art]
-        d = d.set_index('DATE_DECADE').reindex(range(1,37), fill_value=0).reset_index()
+    for tx, cnt in species.items():
+        st.write("Phänologie der Gesangsmeldungen (Aug-Dez) für", tx, ".")
+        d = df[df.TAXON==tx]
+        d = d[d.DATE_DECADE>=22] # Aug-Dez
+        d = d.set_index('DATE_DECADE').reindex(range(22,37), fill_value=0).reset_index()
         st.bar_chart(
             d,
             x = 'DATE_DECADE',
             y = 'OBSERVATIONS'
         )
-        
+    
     
 
 
@@ -102,10 +114,6 @@ with tab4:
 
 # df = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [37.76, -122.4],columns=['lat', 'lon'])
 # st.map(df)
-
-
-
-
 
 #st.checkbox('yes')
 #st.button('Click')
